8c81928b0566616c5ab3d60260b52d50bfa9ca14
Shahrooz Sabet <shahrooz.sabet@gmail.com>
2022-07-25 17:21:03 +0200

    microutils:kotlin-logging added.

--- course-catalog-service/build.gradle.kts
+++ course-catalog-service/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    //logging
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
