5a65dee46ea9a8bef9086fb69f446baf38e8e8d3
Shahrooz <shahrooz.sabet@sinch.com>
2022-10-03 11:23:17 +0200

    mac A1 needed dep.

--- course-catalog-service/build.gradle.kts
+++ course-catalog-service/build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.netty:netty-resolver-dns-native-macos:4.1.82.Final:osx-aarch_64")
 
     //validator
     implementation("org.springframework.boot:spring-boot-starter-validation")
